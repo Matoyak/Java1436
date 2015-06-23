ce0e585c7d8ffec30a5c8deeefa293f7a9e71858
Mason <mwilliam09@gmail.com>
2015-06-22 20:42:26 -0500

    Finished Assignment 2

--- Assignment2/src/main/java/Assignment2.java
+++ Assignment2/src/main/java/Assignment2.java
@@ -4,6 +4,8 @@ import javax.swing.*;
  * Created by Mason Worrell on 6/17/2015.
  */
 public class Assignment2 {
+    private boolean DEBUG = false;
+
     String name, age_s, costSemester_s, costClass_s, gradSemester_s;
     int age;
     double costSemester, costClass;
@@ -16,7 +18,6 @@ public class Assignment2 {
     }
 
     private void userInputs() {
-
         name = JOptionPane.showInputDialog(null, "Hello. What is your name?");
         JOptionPane.showMessageDialog(null,"Welcome, " + name + ".");
 
@@ -31,11 +32,14 @@ public class Assignment2 {
 
         do {
             gradSemester_s = JOptionPane.showInputDialog(null, "Do you graduate this semester?");
-            gradSemester_s.toLowerCase();
-            if (gradSemester_s == "n" || gradSemester_s == "no" || gradSemester_s == "false" || gradSemester_s == "f" || gradSemester_s == "0") {
+            gradSemester_s = gradSemester_s.toLowerCase();
+            //System.out.println(gradSemester_s); //debug
+            if (gradSemester_s.equals("n") || gradSemester_s.equals("no") || gradSemester_s.equals("false")
+                    || gradSemester_s.equals("f") || gradSemester_s.equals("0")) {
                 gradSemester = false;
                 flag = false;
-            } else if (gradSemester_s == "y" || gradSemester_s == "yes" || gradSemester_s == "true" || gradSemester_s == "t") {
+            } else if (gradSemester_s.equals("y") || gradSemester_s.equals("yes") || gradSemester_s.equals("true")
+                    || gradSemester_s.equals("t")) {
                 gradSemester = true;
                 flag = false;
             } else {
@@ -43,12 +47,28 @@ public class Assignment2 {
             }
         } while (flag);
 
-        //debug(name, age, costClass, costSemester, gradSemester);
+        if (DEBUG) {
+            debug(name, age, costClass, costSemester, gradSemester);
+        }
     }
 
     private void output(Assignment2 obj) {
-        System.out.println(obj.age);
-        //stuff
+        if(obj.gradSemester) {
+            JOptionPane.showMessageDialog(null, name + "'s data: \n"
+            + "\tAge: " + obj.age + "\n"
+            + "\tAmount Paid (Total): $" + obj.costSemester + "\n"
+            + "\tAmount Paid (This Class): $" + obj.costClass + "\n"
+            + "\tAmount Paid (Other Classes): $" + (obj.costSemester - obj.costClass) + "\n"
+            + "Congratulations on your upcoming graduation!");
+        } else {
+            JOptionPane.showMessageDialog(null, name + "'s data: \n"
+            + "\tAge: " + obj.age + "\n"
+            + "\tAmount Paid (Total): $" + obj.costSemester + "\n"
+            + "\tAmount Paid (This Class): $" + obj.costClass + "\n"
+            + "\tAmount Paid (Other Classes): $" + (obj.costSemester - obj.costClass) + "\n"
+            + "Continue working hard towards your eventual graduation!");
+        }
+        //When the chance arrives, attempt to display in table.
     }
 
     private static void debug(String name, int age, double costClass, double costSemester, boolean gradSemester) {
