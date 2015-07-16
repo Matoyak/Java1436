5ce5c7ac05d64d3b1d34d62bc941a980bd7b90b0
Mason <mwilliam09@gmail.com>
2015-07-16 16:31:26 -0500

    Finished Assignment 9

--- Assignment9/src/main/java/CarManager/MyCars.java
+++ Assignment9/src/main/java/CarManager/MyCars.java
@@ -8,13 +8,19 @@ import java.util.List;
  * Created by Mason Worrell on 7/15/2015.
  */
 public class MyCars {
-
     public static void main(String[] args) {
         List<Car> cars = new ArrayList<>();
-        Car car1 = new Car();
-        cars.add(car1);
-        Car car2 = new Car("Dodge","Mustang");
-        cars.add(car2);
+        boolean flag = true;
+        do {
+            String make = JOptionPane.showInputDialog("What is the make of your car?");
+            String model = JOptionPane.showInputDialog("What model of car do you own?");
+            Car car = new Car(make,model);
+            cars.add(car);
+            int confirm = JOptionPane.showConfirmDialog(null, "Do you wish to add more cars?");
+            if (confirm == JOptionPane.NO_OPTION || confirm == JOptionPane.CANCEL_OPTION) {
+                flag = false;
+            }
+        }while(flag);
         display(cars);
     }
 
